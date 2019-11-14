e7297b59f58b1f66e57a217e0faab08349b8b68c
Fernando Carrascal Gallego <fernando.carrascal.gal@gmail.com>
2019-11-14 12:20:10 +0100

    Añadido paquete utilidades y switch del menú principal

--- Calculadora/src/principal/Ppal.java
+++ Calculadora/src/principal/Ppal.java
@@ -1,10 +1,41 @@
 package principal;
 
+import utilidades.Leer;
+
 public class Ppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int boton=0;
+		
+		System.out.println("Bienvenido, con este programa podras usar la calculadora.");
+		System.out.println("Para sumar pulse 1.");
+		System.out.println("Para restar pulse 2.");
+		System.out.println("Para miltiplicar pulse 3.");
+		System.out.println("Para dividir pulse 4.");
+		boton=Leer.datoInt();
+		
+		switch (boton) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 0:
+			
+			break;
+			
+		default:
+			System.out.println("No exixte esta opción.");
+			break;
+		}
 	}
 
 }
